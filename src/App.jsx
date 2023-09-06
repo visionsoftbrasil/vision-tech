c9036855cf1822095670680539be73922b6bbd1a
Guilherme Molari <yguilz123@gmail.com>
2023-09-06 19:13:31 +0000

    starting project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<> Teste </>}/>
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
