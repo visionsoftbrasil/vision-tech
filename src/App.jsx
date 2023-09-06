aac1624deb55eb5ec5a37eefa51cf1d4726c3dd6
Guilherme Molari <yguilz123@gmail.com>
2023-09-06 19:45:07 +0000

    griding page, header and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import NavBar from './components/NavBar'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path='/' element={<> Teste </>}/>
-      </Routes>
+      <NavBar />
+      <main>
+        <Routes>
+            <Route exact path='/' element={<Home />}/>
+        </Routes>
+      </main>
+      <Footer />
     </Router>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.sass'
 
-ReactDOM.createRoot(document.querySelector('body')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
 )
