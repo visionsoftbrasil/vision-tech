fb22523e3a7f88d08210ac1c065c552494228e4d
Guilherme Molari <yguilz123@gmail.com>
2023-09-07 09:00:27 -0300

    coming soon finalized

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,26 @@
 import { Link } from "react-router-dom"
+import logo from "../assets/images/logo/central-white-nobg.png"
 
 export default function Header() {
 
     return (
         <header className="header">
             <div className="container">
-                <Link to="/home" className="logo">
-                    Seu Logo
+                <Link to="/" className="logo">
+                    <img src={logo} alt="Logo Vision Software" title="Ir para página inicial"/>
                 </Link>
                 <nav>
                     <ul className="menu">
-                        <li>
+                        <li title="Ir para página inicial">
                             <Link to="/">Página Inicial</Link>
                         </li>
-                        <li>
+                        <li title="Ir para página sobre">
                             <Link to="/about">Sobre</Link>
                         </li>
-                        <li>
+                        <li title="Ir para página de serviços">
                             <Link to="/services">Serviços</Link>
                         </li>
-                        <li>
+                        <li title="Ir para página de contato">
                             <Link to="/contact">Contato</Link>
                         </li>
                     </ul>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,31 @@
+import git from '../assets/svgs/icons/github.svg'
+import insta from '../assets/svgs/icons/instagram.svg'
 
-export default function Home(){
+
+export default function Home() {
 
     return (
-        <>
-        </>
+        
+        <section className="coming-soon">
+            <h1 className="coming">
+                Em breve...
+            </h1>
+            <h2>
+                Verifique nossas redes sociais!
+            </h2>
+            <div className="menu">
+                <a href='https://github.com/visionsoftbrasil' target='_blank'>
+                    <div className="opt">
+                        <img src={git} alt="" />
+                    </div>
+                </a>
+                <a href="https://www.instagram.com/vision.tech.br/" target='_blank'>
+                    <div className="opt">
+                        <img src={insta} alt="" />
+                    </div>
+                </a>
+            </div>
+        </section>
+        
     )
 }
