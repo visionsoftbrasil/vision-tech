f11a3c16bb27b5ba50d9ff8929d3eb13dcfde266
Guilherme Molari <yguilz123@gmail.com>
2023-10-02 11:31:25 +0000

    start menu hamburger

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,9 @@
+import { useState } from "react"
+import menu from '../assets/svgs/icons/menu.svg'
 
 export default function Header() {
+    const [active, setActive] = useState(false)
+
     return (
         <header className="header">
             <div className="container">
@@ -20,6 +24,28 @@ export default function Header() {
                     </ul>
                     <div className="line"></div>
                 </nav>
+            </div>
+            <div className="sand" >
+                <nav className="sand-nav">
+                    {
+                        active ? <ul className="menu">
+                            <li title="Ir para página inicial">
+                                <a href="#pg-inicial">Página Inicial</a>
+                            </li>
+                            <li title="Ir para página sobre">
+                                <a href="#sobre">Sobre Nós</a>
+                            </li>
+                            <li title="Ir para página de serviços">
+                                <a href="#services">Serviços</a>
+                            </li>
+                            <li title="Ir para página de contato">
+                                <a href="#contato">Contato</a>
+                            </li>
+                        </ul>
+                        : ''
+                    }
+                </nav>
+                <img src={menu} onClick={() => setActive(!active)} className="icon-sand" />
             </div>
         </header>
     )
