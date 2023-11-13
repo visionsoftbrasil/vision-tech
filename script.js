29ab4c11aa0a40ba69150a8c354a41f28573fde6
Guilherme Molari <yguilz123@gmail.com>
2023-11-13 20:31:40 -0300

    16:9 carousel finalized

--- script.js
+++ script.js
@@ -7,6 +7,10 @@ function main(){
     const menu = document.getElementById('menu')
     const links = document.getElementsByClassName('links')[0]
     const imgSlide = document.getElementById('imgSlide')
+    const next = document.getElementById('next')
+    const prev = document.getElementById('prev')
+    const slides = document.getElementsByClassName('slide')
+    const containerSlides = document.getElementById('containerSlides')
 
     let menuOpen = false
 
@@ -27,10 +31,10 @@ function main(){
     const interval = setInterval(() => {
     if (auto) nextSlide() 
     else return
-    }, 3000)
+    }, 5000)
 
-    for(let i = 0; i < slides.children.length; i++){
-        dist.push((imgSlide[0].offsetWidth+64)*i)
+    for(let i = 0; i < slides.length; i++){
+        dist.push((imgSlide.offsetWidth+32)*i)
     }
 
     next.addEventListener('click', () => {
@@ -40,7 +44,7 @@ function main(){
             if (auto) {
                 return
             }else auto = true
-        }, 5000)
+        }, 10000)
     })
 
     prev.addEventListener('click', () => {
@@ -50,7 +54,7 @@ function main(){
             if (auto) {
                 return
             }else auto = true
-        }, 5000)
+        }, 10000)
     })
 
     function handleMenu() {
@@ -61,126 +65,24 @@ function main(){
         }
         menuOpen = !menuOpen
     }
+
+    function nextSlide(){
+        counter += 1
+        if (counter >= slides.length) {
+            containerSlides.style.transform = `translateX(${dist[0]}px)`
+            counter = 0
+            return
+        }
+        containerSlides.style.transform = `translateX(-${dist[counter]}px)`
+    }
+
+    function prevSlide(){
+        if (counter == 0) {
+            containerSlides.style.transform = `translateX(-${dist[dist.length-1]}px)`
+            counter = dist.length-1
+            return
+        }
+        counter -= 1
+        containerSlides.style.transform = `translateX(-${dist[counter]}px)`
+    }
 }
-
-
-// const slides = document.getElementById('container-slides')
-// const next = document.getElementById('next')
-// const prev = document.getElementById('prev')
-// const menu = document.getElementById('menu')
-// const nav = document.querySelector('nav')
-// const img = document.getElementById('header-icon')
-// const imgEvent = slides?.querySelectorAll('img')
-// const logoMenu = document.getElementById('logo-menu')
-// const li = nav.querySelectorAll('li')
-// const card = document.getElementsByClassName('card')
-// const ev = document.getElementById('eventos')
-// const drinks = document.getElementById('drinks')
-// const header = document.querySelector('header')
-// const width = screen.width
-// const height = screen.height
-
-// let values = []
-// let menuOpen = false
-
-// let dist = []
-// let counter = 0
-
-// let auto = true
-
-// const interval = setInterval(() => {
-//     if (auto) nextSlide() 
-//     else return
-// }, 3000)
-
-// ev.style.height = header.offsetHeight*1.5+'px'
-// drinks.style.height = header.offsetHeight*1.5+'px'
-
-// function handleMenu() {
-//     if (!menuOpen) {
-//         nav.classList.add('more-width')     
-//         img.src = "./assets/icons/x.svg"
-//     }else {
-//         nav.classList.remove('more-width')
-//         img.src = "./assets/icons/menu.svg"
-//     }
-//     menuOpen = !menuOpen
-// }
-
-// function handleCard(e){
-//     let me = e.target
-//     if (me.classList[0] !== 'card') me = me.parentNode
-//     if (me.classList[0] !== 'card') me = me.parentNode
-//     if (me.classList[0] !== 'card') me = me.parentNode
-//     let desc = me.children[1]
-//     if (!values[me.id]) {
-//         desc.hidden = values[me.id]
-//         values[me.id] = !values[me.id]
-//         desc.scrollIntoView({block: 'center'})
-//         return
-//     }
-//     desc.scrollIntoView({block: 'center'})
-//     desc.hidden = values[me.id]
-//     values[me.id] = !values[me.id]
-// }
-
-// menu.addEventListener("click", handleMenu)
-
-// window.addEventListener('load', () => {
-//     for(let i in li){
-//         if (typeof li[i] == 'object')
-//         li[i].addEventListener('click', handleMenu)
-//     }
-
-//     for(let i in card){
-//         if (typeof card[i] == 'object') {
-//             card[i].addEventListener('click', handleCard)
-//             card[i].id = i
-//             values.push(false)
-//         }
-//     }
-
-//     for(let i = 0; i < slides.children.length; i++){
-//         dist.push((imgEvent[0].offsetWidth+64)*i)
-//     }
-// })
-
-// next.addEventListener('click', () => {
-//     nextSlide()
-//     auto = false
-//     setTimeout(() => {
-//         if (auto) {
-//             return
-//         }else auto = true
-//     }, 5000)
-// })
-
-// prev.addEventListener('click', () => {
-//     prevSlide()
-//     auto = false
-//     setTimeout(() => {
-//         if (auto) {
-//             return
-//         }else auto = true
-//     }, 5000)
-// })
-
-// function nextSlide(){
-//     counter += 1
-//     if (counter >= slides.children.length) {
-//         slides.style.transform = `translateX(${dist[0]}px)`
-//         counter = 0
-//         return
-//     }
-//     slides.style.transform = `translateX(-${dist[counter]}px)`
-// }
-
-// function prevSlide(){
-//     if (counter == 0) {
-//         slides.style.transform = `translateX(-${dist[dist.length-1]}px)`
-//         counter = dist.length-1
-//         return
-//     }
-//     counter -= 1
-//     slides.style.transform = `translateX(-${dist[counter]}px)`
-// }
